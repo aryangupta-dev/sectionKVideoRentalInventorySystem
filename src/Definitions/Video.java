c6b23ac8e92ebf52614c09ef53c251a955176fbb
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-08-29 11:56:49 +0530

    create Video.java

---
+++ src/Definitions/Video.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 8/29/2020
+ *   Time: 11:56 AM
+ *   File: Video.java
+ */
+
+package Definitions;
+
+public class Video {
+}
