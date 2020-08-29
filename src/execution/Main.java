a87f760b6d32b32e571b8df3e3f9c298e39e69a0
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-08-29 11:51:20 +0530

    create Main.java

---
+++ src/execution/Main.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 8/29/2020
+ *   Time: 11:48 AM
+ *   File: Main.java
+ */
+
+package execution;
+
+public class Main {
+}
